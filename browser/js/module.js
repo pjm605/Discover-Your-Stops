f83bae66fd9fde2cf90d091a37583402b5ca77be
Janice Park <pjm605@gmail.com>
2016-09-22 19:03:07 -0400

    first commit

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,7 +1,7 @@
 
 'use strict';
 
-var app = angular.module('myApp', ['vsGoogleAutocomplete']);
+var app = angular.module('myApp', ['vsGoogleAutocomplete', 'ui.router']);
 
 app.run(function ($rootScope) {
   $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
@@ -9,52 +9,59 @@ app.run(function ($rootScope) {
   });
 });
 
-app.controller('MainCtrl', function($scope, MainFactory, $log) {
+app.controller('MainCtrl', function($scope, MainFactory, $log, $state) {
   
   $scope.options = {
     types: ['(cities)'],
     componentRestrictions: { country: 'FR' }
   };
 
+$scope.hello = "hello"
 
   $scope.search = function (from, to) {
   	var cFrom = from.split(",")[0]
   	var cTo = to.split(",")[0]
 
-  	MainFactory.getResult(cFrom, cTo)
-  	.then(function () {
-  		alert("Succeeeeeeeeeed")
-  	})
-  	.catch($log.error)
+  	$state.go('result', {cFrom: cFrom, cTo: cTo})
 
-  	console.log("This is from   " + cFrom  + "   This is to " + cTo)
-  }
+  	// MainFactory.getResult(cFrom, cTo)
+  	// .then(function (results) {
+  	
+  	// 	$scope.results = results
+  	// 	console.log($scope.results)
+  		
+  	// 	$state.go('result')
+  	// })
+  	// .catch($log.error)
+  };
   
 });
 
-app.factory('MainFactory', function ($http, $log) {
+app.service('MainFactory', function ($http, $log) {
+	
+
 	return {
 		getResult: function (from, to) {
 			return $http.get('/api/result/?dName=' + from + "&oName=" + to)
 			.then(function (result) {
-				console.log(result.data)
+				return result.data
 			})
 		}
 	}
 
 });
 
-// app.config(function ($stateProvider) {
+app.config(function ($stateProvider) {
 
-//   $stateProvider.state('result', {
-//     url: '/result',
-//     templateUrl: '/js/result/result.html'
-//     // controller: 'ResultCtrl',
-//     // resolve: {
-//     //   allAlbums: function (AlbumFactory) {
-//     //     return AlbumFactory.fetchAll();
-//     //   }
-//     // }
-//   });
+  $stateProvider.state('result', {
+    url: '/result/:cFrom/:cTo',
+    templateUrl: '/js/result/result.html',
+    controller: 'MainCtrl'
+    resolve: {
+      results: function (MainFactory, $stateParams) {
+        return MainFactory.getResult($stateParams.cFrom, $stateParams.cTo);
+      }
+    }
+  });
 
-// });
+});
