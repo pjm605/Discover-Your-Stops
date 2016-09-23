34c429f64288e38a16a4158cfc488bedf0a94bca
Janice Park <pjm605@gmail.com>
2016-09-22 21:15:05 -0400

    result problem solved

--- browser/js/module.js
+++ browser/js/module.js
@@ -16,24 +16,44 @@ app.controller('MainCtrl', function($scope, MainFactory, $log, $state) {
     componentRestrictions: { country: 'FR' }
   };
 
-$scope.hello = "hello"
+  //$scope.results = { res : ['aa', 'bb']}
+
+
 
   $scope.search = function (from, to) {
   	var cFrom = from.split(",")[0]
   	var cTo = to.split(",")[0]
+    //$scope.results.push('cc'); 
+    //$scope.results.res2 = ['cc'];
 
-  	$state.go('result', {cFrom: cFrom, cTo: cTo})
 
-  	// MainFactory.getResult(cFrom, cTo)
-  	// .then(function (results) {
+    
+
+  	//$state.go('result', {cFrom: cFrom, cTo: cTo})
+    console.log($scope.results);
+    
+
+    var promise = MainFactory.getResult(cFrom, cTo);
+
   	
-  	// 	$scope.results = results
-  	// 	console.log($scope.results)
-  		
-  	// 	$state.go('result')
-  	// })
-  	// .catch($log.error)
+  	promise.then(function (results) {
+
+  		//$scope.hello = "hello"
+      //$state.go('result')
+            $scope.hello = "bbboo"
+
+  		$scope.results = results.data
+      console.log(results.data)
+      console.log($scope)
+
+$state.go('result')
+//return results.data
+
+  	})
   };
+
+  //$scope.results = $scope.search;
+
   
 });
 
@@ -43,9 +63,6 @@ app.service('MainFactory', function ($http, $log) {
 	return {
 		getResult: function (from, to) {
 			return $http.get('/api/result/?dName=' + from + "&oName=" + to)
-			.then(function (result) {
-				return result.data
-			})
 		}
 	}
 
@@ -54,14 +71,14 @@ app.service('MainFactory', function ($http, $log) {
 app.config(function ($stateProvider) {
 
   $stateProvider.state('result', {
-    url: '/result/:cFrom/:cTo',
+    url: '/result/',
     templateUrl: '/js/result/result.html',
     controller: 'MainCtrl'
-    resolve: {
-      results: function (MainFactory, $stateParams) {
-        return MainFactory.getResult($stateParams.cFrom, $stateParams.cTo);
-      }
-    }
+    // resolve: {
+    //   results: function (MainFactory, $stateParams) {
+    //     return MainFactory.getResult($stateParams.cFrom, $stateParams.cTo);
+    //   }
+    // }
   });
 
 });
