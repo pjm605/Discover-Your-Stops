8fd665e42290ce98f61204ee250e5dabdb79aaae
Janice Park <pjm605@gmail.com>
2016-09-23 21:07:49 -0400

    link to expedia

--- browser/js/activity/activity.controllers.js
+++ browser/js/activity/activity.controllers.js
@@ -3,7 +3,18 @@ app.controller('ActivityCtrl', function ($scope, MainFactory, $log, $state, $sta
   $scope.location = $stateParams.location
   MainFactory.getStops( $scope.location )
   .then(function (activities) {
+  	
+  	for (var i = 0; i < activities.data.activities.length; i++)
+  	{
+  		var act = activities.data.activities[i];
+  		var name = act.title;
+  		name = name.replace(/[^A-Za-z\d]+/g, '-').toLowerCase()
+  		act["linkTo"] = "https://www.expedia.com/things-to-do/" + name + ".a" + act.id + ".activity-details";
+  	}
     $scope.activities = activities.data.activities
+
+    console.log( $scope.activities)
+
   })
   .catch($log.error)
 
